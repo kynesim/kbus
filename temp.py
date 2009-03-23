caf0a6d655cdcc783cdc81b9c6253f45921c9be8
tony.ibbs <none@none>
2009-03-23 17:01:09 +0000

    Add new File class (and test it in temp.py)
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%4017

--- temp.py
+++ temp.py
@@ -1,48 +1,49 @@
 import os
-from kbus import Message, bind, next_len
-from array import array
+from kbus import Message, File
 
 print 'Opening device f for read/write'
-f = open('/dev/kbus0','wb+')
+f = File(0,'wb+')
+print f
 
 name1 = '$.Fred.Jim'
 name2 = '$.Fred.Bob.William'
 name3 = '$.Fred.Bob.Jonathan'
 
-bind(f,name1,True)     # replier
-bind(f,name2,True)     # replier
-bind(f,name3,False)    # just listener
+f.bind(name1,True)     # replier
+f.bind(name2,True)     # replier
+f.bind(name3,False)    # just listener
 
 msg1 = Message(name1,data='dat1')
-msg1.to_file(f)
-m1 = Message(f.read(next_len(f)))
+f.write(msg1)
+
+m1 = f.read()
 # For message 1, there is no reply needed
 assert not m1.should_reply()
 
 msg2 = Message(name2,data='dat2',flags=Message.WANT_A_REPLY)
-msg2.to_file(f)
-m2 = Message(f.read(next_len(f)))
+f.write(msg2)
+m2 = f.read()
 # For message 2, a reply is wanted, and we are the replier
-print m1
+print m2
 assert m2.should_reply()
 
 msg3 = Message(name3,data='dat3',flags=Message.WANT_A_REPLY)
-msg3.to_file(f)
-m3 = Message(f.read(next_len(f)))
+f.write(msg3)
+m3 = f.read()
 # For message 3, a reply is wanted, and we are not the replier
 assert not m1.should_reply()
-
 
 
 # So, we should reply to message 2 - let's do so
 (id,in_reply_to,to,from_,flags,name,data_array) = msg2.extract()
 
 reply = Message(name, data=None, in_reply_to=id, to=from_)
-reply.to_file(f)
+f.write(reply)
 
 # And we should be able to read it...
-m4 = Message(f.read(next_len(f)))
+m4 = f.read()
 
 assert m4.equivalent(reply)
 
 f.close()
+print f
