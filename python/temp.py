c994caa24c52fa70d12508611a416da3fd3d50ef
tibs@tibsnjoan.co.uk <none@none>
2009-12-15 16:56:39 +0000

    Move the (by now) traditional temporary work file from the kernel module directory.
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40184

---
+++ python/temp.py
@@ -0,0 +1,63 @@
+from __future__ import with_statement
+
+import os, time
+import errno
+from kbus import *
+from test_kbus import check_IOError
+
+from kbus import _struct_from_string
+from kbus import _MessageHeaderStruct
+
+os.system('sudo insmod kbus.ko')
+time.sleep(0.5)
+
+
+try:
+        with KSock(0,'rw') as replier:
+            replier.bind('$.fred',True)
+
+            with KSock(0,'rw') as sender:
+
+                r1 = Request('$.fred','one')
+                r2 = Request('$.fred','two')
+                r3 = Request('$.fred','three')
+
+                sender.write_msg(r1)
+                sender.send()
+
+                len1 = replier.next_msg()
+
+                sender.write_msg(r2)
+
+                m1 = replier.read_msg(len1)
+
+                sender.send()
+
+                x1 = reply_to(m1)
+                replier.write_msg(x1)
+
+                sender.write_msg(r3)
+
+                replier.send()
+
+                sender.send()
+
+                len2 = replier.next_msg()
+
+                m2 = replier.read_msg(len2)
+
+                x2 = reply_to(m2)
+                replier.write_msg(x2)
+
+                try:
+                    replier.send()
+                except:
+                    assert False, "Failed Issue 6 test"
+
+                while 1:
+                    time.sleep(100)
+finally:
+    pass
+    ##os.system('sudo rmmod kbus')
+
+# vim: set tabstop=8 shiftwidth=4 expandtab:
