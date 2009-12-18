457c72366df29a3b81878b8295a24743426c34ff
tibs@tibsnjoan.co.uk <none@none>
2009-12-18 13:18:23 +0000

    A Limpet that listens for KBUS messages...
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40188

--- python/sandbox/limpet.py
+++ python/sandbox/limpet.py
@@ -2,6 +2,7 @@
 """Limpet - a mechanism that proxies KBUS messages to/from another Limpet.
 """
 
+import select
 import sys
 
 from kbus import KSock
@@ -41,16 +42,58 @@ class Limpet(object):
 
         self.ksock = KSock(kbus_device, 'rw')
 
+        self.replier_for = set()
+        self.listener_for = set()
+
+
     def close(self):
         """Tidy up when we're finished.
         """
         self.ksock.close()
+        print 'Limpet closed'
 
     def __repr__(self):
         return 'Limpet(%s, %s)'%(self.kbus_device, repr(self.sock_address))
 
     def __str__(self):
         return 'Limpet from KSock %d via %s'%(self.kbus_device, self.sock_address)
+
+    def bind(self, name, replier=False):
+        """Bind this Limpet to Listen or Reply for the named message.
+
+        'name' is the message name we want to bind, and if 'replier' is true
+        then this Limpet will proxy as a Replier for the message name.
+        """
+        self.ksock.bind(name, replier)
+        if replier:
+            self.replier_for.add(name)
+        else:
+            self.listener_for.add(name)
+
+    def unbind(self, name, replier=False):
+        """Unbind this Limpet from Listening or Replying to the named message.
+
+        'name' is the message name we want to unbind, and if 'replier' is true
+        then this Limpet was proxying as a Replier for the message name.
+        """
+        self.ksock.unbind(name, replier)
+        if replier:
+            self.replier_for.remove(name)
+        else:
+            self.listener_for.remove(name)
+
+    def run_forever(self):
+        """Or, at least, until we're interrupted.
+        """
+        while 1:
+            # Wait for a message written to us, with no timeout
+            # (at least for the moment)
+            (r, w, x) = select.select( [self.ksock], [], [])
+
+            # Since we know that we only queued for one thing, then presumably
+            # that one thing must be all we have to listen for...
+            msg = self.ksock.read_next_msg()
+            print 'Limpet heard',msg
 
     # Implement 'with'
     def __enter__(self):
@@ -69,10 +112,12 @@ class Limpet(object):
 
 
 def main(args):
-    """Do whatever needs doing.
+    """Run a Limpet. Use kmsg to send messages (via KBUS) to it...
     """
     with Limpet(0, '/tmp/limpet0') as l:
         print l
+        l.bind('$.*')
+        l.run_forever()
 
 if __name__ == "__main__":
     args = sys.argv[1:]
