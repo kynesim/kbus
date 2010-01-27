0da396ca071cb4c31fd4a67ca70b2a70c0ad1b88
tibs@tibsnjoan.co.uk <none@none>
2010-01-27 11:33:05 +0000

    Add EAGAIN, improve/simplify KBUS documentation text handling.
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40243

--- utils/errno.py
+++ utils/errno.py
@@ -10,73 +10,129 @@ in KBUS.
 
 import sys
 import os
+import textwrap
 import errno
 from errno import errorcode
-from textwrap import wrap, fill
 
 def __(text):
-    return fill(text, 80)
+    return textwrap.dedent(text)
 
 kbus_codes = {
-    'EADDRINUSE'    : __('On attempting to bind a message name as replier:' \
-                      ' There is already a replier bound for this message'),
-    'EADDRNOTAVAIL' : __('On attempting to send a Request message:'\
-                      ' There is no replier bound for this message\'s name.') + \
-                      '\n\n' + \
-                      __('On attempting to send a Reply message: The sender of' \
-                      ' the original request (i.e., the KSock mentioned as the' \
-                      ' "to" in the Reply) is no longer connected.'),
-    'EALREADY'      : __('On attempting to write to a KSock, when a previous send has'\
-                      ' returned EAGAIN: Either DISCARD the message, or use select/poll'\
-                      ' to wait for the send to complete, and write to be allowed.'),
-    'EBADMSG'       : __('On attempting to bind, unbind or send a message:'\
-                      'The message name is not valid. On sending, this can also be'\
-                      ' because the message name is a wildcard.'),
-    'EBUSY'         : __('On attempting to send, then:\n' \
-                      ' For a request, the replier\'s message queue is full.\n'\
-                      ' For any message, with ALL_OR_FAIL set, one of the targetted'\
-                      ' listener/replier queues was full.'),
-    'ECONNREFUSED'  : __('On attempting to send a Reply, the intended recipient'\
-                      ' (the notional original sender of the Request) is not expecting'\
-                      ' a Reply with that message id in its "in_reply_to". Or, in other'\
-                      ' words, this appears to be an attempt to reply to the wrong message'\
-                      ' id or the wrong KSock.'),
-    'EINVAL'        : 'Something went wrong (generic error).',
-    'EMSGSIZE'      : __('On attempting to write or send a message:'\
-                      ' The message data is too long.'),
-    'ENAMETOOLONG'  : __('On attempting to bind, unbind or send a message: '\
-                      'The message name is too long.'),
-    'ENOENT'        : __('On attempting to open a KSock:'\
-                      ' There is no such device (normally because one has tried to open,'\
-                      ' for instance, "/dev/kbus9" when there are only 3 KBUS devices).'),
-    'ENOLCK'        : __('On attempting to send a Request, when there is not enough room'\
-                      ' in the sender\'s message queue to guarantee that it can receive'\
-                      ' a reply for every Request already sent, plus this one. If there'\
-                      ' are oustanding messages in the sender\'s message queue, then the'\
-                      ' solution is to read some of them. Otherwise, the sender will have'\
-                      ' to wait until one of the Repliers replies to a previous Request'\
-                      ' (or goes away and KBUS replies for it).') + \
-                      '\n\n' + \
-                      __('When this error is received, the send has failed (just as if the'\
-                      ' message was invalid). The sender is not left in "sending" state,'\
-                      ' nor has the message been assigned a message id.') + \
-                      '\n\n' + \
-                      __('Note that this is not EAGAIN, since we do not want to block the'\
-                      ' sender (in the SEND) if it is up to the sender to perform a read'\
-                      ' to sort things out.'),
-    'ENOMSG'        : __('On attempting to send, when there is no message waiting to be'\
-                      ' sent (either because there has been no write since the last'\
-                      ' send, or because the message being written has been discarded).'),
-    'EPIPE'         : __('On attempting to send *to* a specific replier, the replier with'\
-                      ' that id is no longer bound to the given message\'s name.'),
+        # Things cut-and-pasted (and mildly mangled) from the KBUS documentation
+    'EADDRINUSE': __("""\
+        On attempting to bind a message name as replier: There is already a
+        replier bound for this message.
+        """),
+
+    'EADDRNOTAVAIL': __("""\
+        On attempting to send a Request message: There is no replier bound for
+        this message's name.
+
+        On attempting to send a Reply message: The sender of the original
+        request (i.e., the KSock mentioned as the ``to`` in the Reply) is no
+        longer connected.
+        """),
+
+    'EALREADY': __("""\
+        On attempting to write to a KSock, when a previous send has returned
+        EAGAIN. Either DISCARD the message, or use select/poll to wait for the
+        send to complete, and write to be allowed.
+        """),
+
+    'EBADMSG': __("""\
+        On attempting to bind, unbind or send a message: The message name is
+        not valid. On sending, this can also be because the message name is a
+        wildcard.
+        """),
+
+    'EBUSY': __("""\
+        On attempting to send, then:
+
+        1. For a request, the replier's message queue is full.
+
+        2. For any message, with ALL_OR_FAIL set, one of the targetted
+           listener/replier queues was full.
+        """),
+
+    'ECONNREFUSED': __("""\
+        On attempting to send a Reply, the intended recipient (the notional
+        original sender of the Request) is not expecting a Reply with that
+        message id in its 'in_reply_to'. Or, in other words, this appears to be
+        an attempt to reply to the wrong message id or the wrong KSock.
+        """),
+
+    'EINVAL': __("""\
+        Something went wrong (generic error).
+        """),
+
+    'EMSGSIZE': __("""\
+        On attempting to write or send a message: The message data is too
+        long.
+        """),
+
+    'ENAMETOOLONG': __("""\
+        On attempting to bind, unbind or send a message: The message name is
+        too long.
+        """),
+
+    'ENOENT': __("""\
+        On attempting to open a KSock: There is no such device (normally
+        because one has tried to open, for instance, '/dev/kbus9' when there
+        are only 3 KBUS devices).
+        """),
+
+    'ENOLCK': __("""\
+        On attempting to send a Request, when there is not enough room in the
+        sender's message queue to guarantee that it can receive a reply for
+        every Request already sent, *plus* this one. If there are oustanding
+        messages in the sender's message queue, then the solution is to read
+        some of them. Otherwise, the sender will have to wait until one of the
+        Repliers replies to a previous Request (or goes away and KBUS replies
+        for it).
+
+        When this error is received, the send has failed (just as if the
+        message was invalid). The sender is not left in "sending" state, nor
+        has the message been assigned a message id.
+
+        Note that this is *not* EAGAIN, since we do not want to block the
+        sender (in the SEND) if it is up to the sender to perform a read to
+        sort things out.
+        """),
+
+    'ENOMSG': __("""\
+        On attempting to send, when there is no message waiting to be sent
+        (either because there has been no write since the last send, or because
+        the message being written has been discarded).
+        """),
+
+    'EPIPE': __("""\
+        On attempting to send 'to' a specific replier, the replier with that id
+        is no longer bound to the given message's name.
+        """),
 
     # Things not explicit in the KBUS documentation
-    'EFAULT'        : __('Memory allocation, copy from user space, or other such failed.' \
-                         ' This is normally very bad, it should not happen, UNLESS it is' \
-                         ' the result of calling an ioctl, when it indicates that the'
-                         ' ioctl argument cannot be accessed.'),
-    'ENOMEM'        : __('Memory allocation failed (return NULL).' \
-                         ' This is normally very bad, it should not happen.'),
+    'EFAULT': __("""\
+            Memory allocation, copy from user space, or other such failed. This
+            is normally very bad, it should not happen, UNLESS it is the result
+            of calling an ioctl, when it indicates that the ioctl argument
+            cannot be accessed.
+            """),
+
+    'ENOMEM': __("""\
+            Memory allocation failed (return NULL). This is normally very bad,
+            it should not happen.
+            """),
+
+    'EAGAIN': __("""\
+            On attempting to send, the message being sent had ALL_OR_WAIT set,
+            and one of the targetted listener/replier queues was full.
+
+            On attempting to unbind when Replier Bind Events have been
+            requested, one or more of the KSocks bound to receive
+            "$.KBUS.ReplierBindEvent" messages has a full message queue,
+            and thus cannot receive the unbind event. The unbind has not been
+            done.
+            """),
     }
 
 def check_kbus(errname):
