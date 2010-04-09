d7c1483cb6a102d45583daa3f6daeda60bd30cf5
tibs@tibsnjoan.co.uk <none@none>
2010-04-09 20:00:58 +0000

    Oops - fix some of the licencese that should just be MPL 1.1
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40337

--- libkbus/limpet.c
+++ libkbus/limpet.c
@@ -28,16 +28,6 @@
  *   Kynesim, Cambridge UK
  *   Tony Ibbs <tibs@tonyibbs.co.uk>
  *
- * Alternatively, the contents of this file may be used under the terms of the
- * GNU Public License version 2 (the "GPL"), in which case the provisions of
- * the GPL are applicable instead of the above.  If you wish to allow the use
- * of your version of this file only under the terms of the GPL and not to
- * allow others to use your version of this file under the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL.  If you do not delete the
- * provisions above, a recipient may use your version of this file under either
- * the MPL or the GPL. 
- *
  * ***** END LICENSE BLOCK *****
  */
 
