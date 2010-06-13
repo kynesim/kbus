f5553f4c0ee7187d40fc10c9bdcb90176aaeaffa
tibs@tibsnjoan.co.uk <none@none>
2010-06-13 19:34:12 +0000

    OK, this would appear to be an *exceedingly hacky* approach to running my multiple interpreters, but one that actually works...
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40344

--- python/sandbox/sequence.py
+++ python/sandbox/sequence.py
@@ -4,11 +4,16 @@
 It attempts to output the result in a form that can be cut-and-pasted
 straight into the reStructuredText document.
 
-Note that the handling of output indentation is nastily hacky!
+Note that just about everything about this script is exceedingly hacky...
 """
 
+import os
+import fcntl
+import errno
+import signal
 import sys
-from code import InteractiveConsole
+import subprocess
+import time
 
 INTRO = """
   .. compound::
@@ -18,8 +23,7 @@ INTRO = """
 """
 
 INDENT  = "       "
-PROMPT1 = ">>> %s"
-PROMPT2 = "... %s"
+DEFAULT_WAIT = 0.1
 
 def indenter(text):
     lines = text.split('\n')
@@ -31,7 +35,7 @@ def indenter(text):
             indented.append('')
     return '\n'.join(indented)
 
-class Outputter():
+class Outputter(object):
 
     def write(self, text, indent=True):
         if indent:
@@ -39,32 +43,7 @@ class Outputter():
         else:
             sys.__stdout__.write(text)
 
-class Console(InteractiveConsole):
-
-    def __init__(self, index, name, *args, **kwargs):
-        InteractiveConsole.__init__(self, *args, **kwargs)
-
-        self.name = name
-        self.index = index
-        self.needsmore = False
-
-        # The following leaves us having "implicitly" imported sys,
-        # so far as the user is concerned. Oh well.
-        self.push("import sys; sys.stdout=outputter")
-
-    def write(self, data):
-        sys.__stdout__.write(indenter(data))
-
-    def do(self, lines):
-        sys.stdout.write(INTRO%(self.index, self.name), indent=False)
-        for line in lines:
-            if self.needsmore:
-                print PROMPT2%line
-            else:
-                print PROMPT1%line
-            self.needsmore = self.push(line)
-
-class Terminal():
+class Terminal(object):
     """This class represents one "terminal" in our example.
 
     Or, putting it another way, one of the "Actors" in our play...
@@ -72,26 +51,98 @@ class Terminal():
     A terminal has a name (the actor name) and an index (the terminal number).
     """
 
-    def __init__(self, index, name, globals=None):
+    def __init__(self, index, name, *args, **kwargs):
+        self.interp = subprocess.Popen('python -u -i -',
+                                       shell=True,
+                                       stdin=subprocess.PIPE,
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.STDOUT,
+                                       )
         self.name = name
         self.index = index
-        if globals is None:
-            self.globals = {}
+
+        # Make the process's standard output non-blocking
+        fl = fcntl.fcntl(self.interp.stdout, fcntl.F_GETFL)
+        fcntl.fcntl(self.interp.stdout, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
+    def read(self, wait=DEFAULT_WAIT):
+        if wait:
+            time.sleep(wait)
+        try:
+            self.interp.stdin.flush()
+            stuff = self.interp.stdout.read()
+            new = []
+            lines = stuff.split('\n')
+            for line in lines:
+                new.append('%s%s'%(INDENT, line))
+            lines = '\n'.join(new)
+            sys.stdout.write(lines)
+            return False
+        except IOError as exc:
+            if exc.errno == errno.EAGAIN:
+                return True
+            else:
+                raise
+
+    def do(self, *lines, **kwargs):
+        """
+
+        * 'wait' is seconds to wait before each read
+        * if 'output' (default, True) then we expect output from our
+          command(s), otherwise we do not...
+        """
+        if 'wait' in kwargs:
+            wait = kwargs['wait']
         else:
-            self.globals = globals
+            wait = DEFAULT_WAIT
+        if 'output' in kwargs:
+            output = kwargs['output']
+        else:
+            output = True
+        sys.stdout.write(INTRO%(self.index, self.name))
+        for line in lines:
+            if self.read(wait):     # read any left-over output, and the prompt
+                sys.stdout.write('%s>>> '%INDENT)
+            self.interp.stdin.write('%s\n'%line)
+            self.interp.stdin.flush()
+            print line
+            if output:
+                self.read(wait)
+        sys.stdout.write('\n')
 
-        if "outputter" not in self.globals:
-            self.globals["outputter"] = Outputter()
-
-        self.console = Console(index, name,
-                               locals=self.globals,
-                               filename='<%s>'%name)
-
-    def do(self, *lines):
-        self.console.do(lines)
+    def control_c(self, wait=DEFAULT_WAIT):
+        time.sleep(wait)
+        # Don't try to read anything, as we assume that we're
+        # trying to break out of (for instance) an infinite loop
+        sys.stdout.write('%s<CONTROL-C>\n'%INDENT)
+        self.interp.send_signal(signal.SIGINT)
+        self.read(wait)
+        sys.stdout.write('\n')
 
 
 def main():
+
+    if 0:
+        x = subprocess.Popen('python -u -i -', shell=True, stdin=subprocess.PIPE)
+        x.stdin.write('import os\n')
+        x.stdin.write('dir(os)\n')
+        x.stdin.flush()
+        time.sleep(2)
+        x.send_signal(signal.SIGINT)
+        print
+        print 'Next...'
+
+    x = Terminal(0, "Test")
+    x.do('import os', ouptut=False)
+    x.do('dir(os)')
+    x.do("import time",
+         "while 1:",
+         "   time.sleep(1)",
+         "", output=False)
+    x.control_c()
+    x.read()
+    return
+
     r = Terminal(1, "Rosencrantz")
     r.do("from kbus import Ksock, Message",
          "rosencrantz = Ksock(0)",
