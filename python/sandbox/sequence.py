9ccdcd0984b09458782bd0c5034dd6edffea5518
tibs@tibsnjoan.co.uk <none@none>
2010-06-14 16:06:09 +0000

    Translated some more of the simple example to sequence.py, including showing an error in the examples...
    --HG--
    extra : convert_revision : svn%3A53f7ac7a-121e-11de-a074-998af0e899fe/trunk%40347

--- python/sandbox/sequence.py
+++ python/sandbox/sequence.py
@@ -188,11 +188,194 @@ def main():
           "   print 'We heard', msg.name, msg.data",
           "")
 
-    r.do("rosencrantz.send_msg(ahem)")
 
+    r.do("rosencrantz.send_msg(Message('$.Actor.Speak', 'Hello there'))",
+         "rosencrantz.send_msg(Message('$.Actor.Speak', 'Can you hear me?'))")
+
+    print
+    print "the audience should be able to hear him:"
     a.do("", prompt=False)
+
+    print
+    print "So now we'll introduce another participant:"
+    g = Terminal(3, "Guldenstern")
+    g.do("from kbus import *",
+         "guildenstern = Ksock(0)",
+         "guildenstern.bind('$.Actor.*')")
+
+    print
+    print "Here, guildenstern is binding to any message whose name starts with"
+    print "``$.Actor.``. In retrospect this, of course, makes sense for the"
+    print "audience, too - let's fix that:"
+
     a.control_c()
-    a.do("print audience.read_next_msg()")
+    a.do("audience.bind('$.Actor.*')",
+         "while 1:",
+         "   msg = audience.wait_for_msg()",
+         "   print 'We heard', msg.name, msg.data",
+         "")
+
+    print
+    print "And maybe rosencrantz will want to hear his colleague:"
+    r.do("rosencrantz.bind('$.Actor.*')")
+
+    print
+    print "So let guildenstern speak:"
+    g.do("guildenstern.send_msg(Message('$.Actor.Speak', 'Pssst!'))",
+         "# Remember guildenstern is also listening to '$.Actor.*'",
+         "print guildenstern.read_next_msg()")
+
+    print
+    print "and rosencrantz hears:"
+    r.do("print rosencrantz.read_next_msg()")
+
+    print
+    print "However, when we look to the audience, we see:"
+    a.do("", prompt=False)
+
+
+    print
+    print "This is because the audience has bound to the message twice - it is hearing it"
+    print "once because it asked to receive every ``$.Actor.Speak`` message, and again"
+    print "because it asked to hear any message matching ``$.Actor.*``."
+    print
+    print "The solution is simple - ask not to hear the more specific version:"
+
+    a.control_c()
+    a.do("audience.unbind('$.Actor.Speak')",
+         "while 1:",
+         "   msg = audience.wait_for_msg()",
+         "   print 'We heard', msg.from_, 'say', msg.name, msg.data",
+         "")
+
+    print """
+Note that we've also amended the printout to say who the message was from.
+Each Ksock connection has an id associated with it - for instance:
+"""
+
+    r.do("rosencrantz.ksock_id()")
+
+    print "\nand every message indicates who sent it, so:"
+
+    r.do("print 'I heard', message.from_, 'say', message.name, message.data")
+
+"""
+We've shown that KBUS allows one to "announce" (or, less politely,
+"shout") messages, but KBUS also supports asking questions. Thus:
+
+  .. compound::
+
+     *Terminal 3: Guildenstern* ::
+
+        >>> guildenstern.bind('$.Actor.Guildenstern.query', True)
+
+allows Guildenstern to bind to this new message name as a Replier.
+
+       *(Only one person may be bound as Replier for a particular message
+       name at any one time, so that it is unambiguous who is expected to do
+       the replying.*
+
+       *Also, if a Sender tries to send a Request, but no-one has bound to that
+       message name as a Replier, then an error is raised (contrast that with
+       ordinary messages, where if no-one is listening, the message just gets
+       ignored).)*
+
+If Rosencrantz then sends a Request of that name:
+
+  .. compound::
+
+     *Terminal 1: Rosencrantz* ::
+
+        >>> req = Request('$.Actor.Guildenstern.query', 'Were you speaking to me?')
+        >>> rosencrantz.send_msg(req)
+        MessageId(0, 6)
+
+Guildenstern can receive it:
+
+  .. compound::
+
+     *Terminal 3: Guildenstern* ::
+
+        >>> msg2 = guildenstern.read_next_msg()
+        >>> print 'I heard', msg2
+        I heard <Request '$.Actor.Guildenstern.query', id=[0:6], from=1, flags=0x3 (REQ,YOU), data='Were you speaking to me?'>
+        >>> msg3 = guildenstern.read_next_msg()
+        >>> print msg3
+        <Request '$.Actor.Guildenstern.query', id=[0:6], from=1, flags=0x1 (REQ), data='Were you speaking to me?'>
+
+As we should expect, guildenstern is getting the message twice, once because
+he has bound as a listener to '$.Actor.*', and once because he is bound as a
+Replier to this specific message.
+
+    *(There is, in fact, a way to ask KBUS to only deliver one copy of
+    a given message, and if guildenstern had used that, he would only have
+    received the Request that was marked for him to answer. I'm still a little
+    undecided how often this mechanism should be used, though.)*
+
+Looking at the two messages, the first is the Request specifically to
+guildenstern, which he is meant to answer:
+
+  .. compound::
+
+     *Terminal 3: Guildenstern* ::
+
+        >>> print msg2.wants_us_to_reply()
+        True
+
+(and that is what the ``YOU`` in the flags means).
+
+And rosencrantz himself will also have received a copy:
+
+  .. compound::
+
+     *Terminal 1: Rosencrantz* ::
+
+        >>> print rosencrantz.read_next_msg()
+        <Request '$.Actor.Guildenstern.query', id=[0:6], from=1, flags=0x1 (REQ), data='Were you speaking to me?'>
+
+Guildenstern can then reply:
+
+  .. compound::
+
+     *Terminal 3: Guildenstern* ::
+
+        >>> reply = reply_to(msg2, 'Yes, I was')
+        >>> print reply
+        <Reply '$.Actor.Guildenstern.query', to=1, in_reply_to=[0:6], data='Yes, I was'>
+        >>> guildenstern.send_msg(reply)
+        MessageId(0, 7)
+
+The ``reply_to`` convenience function crafts a new ``Reply`` message, with the
+various message parts set in an appropriate manner. And thus:
+
+  .. compound::
+
+     *Terminal 1: Rosencrantz* ::
+
+        >>> rep = rosencrantz.read_next_msg()
+        >>> print 'I heard', rep.from_, 'say', rep.name, rep.data
+        I heard 3 say $.Actor.Guildenstern.query Yes, I was
+
+Note that Rosencrantz didn't need to bind to this message to receive it - he
+will always get a Reply to any Request he sends (KBUS goes to some lengths to
+guarantee this, so that even if Guildenstern closes his Ksock, it will
+generate a "gone away" message for him).
+
+And, of course:
+
+  .. compound::
+
+     *Terminal 2: Audience* ::
+
+        We heard 1 say $.Actor.Guildenstern.query Were you speaking to me?
+        We heard 3 say $.Actor.Guildenstern.query Yes, I was
+"""
+
+#    r.do("rosencrantz.send_msg(ahem)")
+
+#    a.do("", prompt=False)
+#    a.control_c()
+#    a.do("print audience.read_next_msg()")
 
 
 if __name__ == '__main__':
