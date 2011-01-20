d14aff2c9182ddaf5c45599d0e94112f7457ba49
Ross Younger <wry@kynesim.co.uk>
2011-01-20 14:08:35 +0000

    Make docs buildable in current layout.  hgignore *.pyc.

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.append(os.path.abspath('.'))
-sys.path.append(os.path.abspath('../kbus'))
+sys.path.append(os.path.abspath('../python'))
 
 # -- General configuration -----------------------------------------------------
 
