4a3203d90d9373736917702f729a4ad3ff5734e1
Rhodri James <rhodri@kynesim.co.uk>
2020-12-08 17:18:21 +0000

    access_ok macro changed in kernal 5.0

--- kbus/kbus_main.c
+++ kbus/kbus_main.c
@@ -4232,14 +4232,12 @@ static long kbus_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 	 * Check our arguments at least vaguely match. Note that VERIFY_WRITE
 	 * allows R/W transfers. Remember that 'type' is user-oriented, while
 	 * access_ok is kernel-oriented, so the concept of "read" and "write"
-	 * is reversed
+	 * is reversed.
+         *
+         * In Linux kernel 5.0, the 'type' parameter was dropped.
 	 */
-	if (_IOC_DIR(cmd) & _IOC_READ)
-		err = !access_ok(VERIFY_WRITE, (void __user *)arg,
-				_IOC_SIZE(cmd));
-	else if (_IOC_DIR(cmd) & _IOC_WRITE)
-		err = !access_ok(VERIFY_READ, (void __user *)arg,
-				_IOC_SIZE(cmd));
+	if (_IOC_DIR(cmd) & (_IOC_READ | _IOC_WRITE))
+		err = !access_ok((void __user *)arg, _IOC_SIZE(cmd));
 	if (err)
 		return -EFAULT;
 
